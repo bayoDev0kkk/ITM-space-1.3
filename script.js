f37a48770368bf159925ffd4cd3c58c040ee34a7
Baiel Kanybekov <kanybekov245@gmail.com>
2024-07-14 18:26:12 +0200

    swiper-slider

--- script.js
+++ script.js
@@ -23,3 +23,63 @@ function toggleReadMore() {
     closeModal.addEventListener('click',() => {
         modal.classList.remove('active')
     })
+
+
+
+
+
+
+    const btn = document.querySelector('.fix-services__read-more');
+    const items = Array.from(document.querySelectorAll('.fix-services__item'));
+    let itemsHidden = false;
+    
+    function responseOnWindowWidth() {
+      if (window.innerWidth > 1119) {
+        items.forEach((item, index) => {
+          if (index > 7) {
+            item.style.display = 'none';
+          }
+        })
+      }
+      else {
+        items.forEach((item, index) => {
+          if (index > 5) {
+            item.style.display = 'none';
+          }
+        })
+      }
+      itemsHidden = true;
+    };
+    responseOnWindowWidth();
+    
+    function readMore() {
+      if (itemsHidden) {
+        items.forEach((item) => {
+          item.style.display = 'flex';
+        });
+        itemsHidden = false;
+        btn.innerHTML = 'Скрыть';
+        btn.style.background = '#f8f8f8 url("img/logo/expand180.svg") no-repeat 0 50%';
+        btn.style.margin = '46px 0 0 0';
+      }
+      else {
+        responseOnWindowWidth();
+        btn.innerHTML = 'Показать всё';
+        btn.style.background = '#f8f8f8 url("img/logo/expand.svg") no-repeat 0 50%';
+        btn.style.margin = '24px 0 0 0';
+      }
+    };
+    
+    function onResize() {
+      items.forEach((item) => {
+        item.style.display = 'flex';
+      });
+      responseOnWindowWidth();
+    }
+    
+    window.addEventListener('resize', onResize);
+
+
+
+
+
